135559cab0409d58e3b15cb171763af148cdd89b
ayariii <90261694+ayariii@users.noreply.github.com>
2022-09-01 12:23:33 +0200

    Add files via upload

---
+++ Ny mapp/Fordon.cs
@@ -0,0 +1,34 @@
+
+
+
+    public class Fordon
+    {
+        private string tillverkare;
+        private string modell;
+        private int årsmodell;
+        private int vikt; 
+
+        public stsring Tillverkare
+        {
+            get{ return tillverkare; }
+            set { tillverkare = value; }
+        }
+
+        public string modell
+        {
+            get { return modell; }
+            set { modell = value; }
+        }
+
+        public int Årsmodell
+        {
+            get { return årsmodell;}
+            set { årsmodell = value;}
+        }
+        public int Vikt
+        {
+            get { return vikt;}
+            set { vikt = value;}
+        }
+
+    }
---
+++ Ny mapp/Program.cs
@@ -0,0 +1,29 @@
+﻿
+class Program{
+    public static void Main(){
+        Fordon a = new Fordon();
+        a.setTillverkare("BMW");
+        a.setModell("a8");
+        a.setVikt(700);
+        a.setÅrsmodell(2022);
+
+     Fordon b = new Fordon();
+        b.setTillverkare("Ford");
+        b.setModell("s8");
+        b.setVikt(500);
+        b.setÅrsmodell(2020);
+
+        Console.WriteLine(a.getTillverkare());
+        Console.WriteLine(a.getModell());
+        Console.WriteLine(a.getVikt());
+        Console.WriteLine(a.getÅrsmodell());
+
+        Console.WriteLine(b.getTillverkare());
+        Console.WriteLine(b.getModell());
+        Console.WriteLine(b.getVikt());
+        Console.WriteLine(b.getÅrsmodell());
+    }
+    
+}
+
+
